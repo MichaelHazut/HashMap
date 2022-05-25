59a5d8de1e0c5873516399125bb0617cec032f7a
Michael <Michael@MICHAELZENBOOK>
2022-05-25 14:21:27 +0300

    Add project files.

---
+++ HashMap.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blossom
+{
+    internal class HashMap
+    {
+        private Dictionary<string, string>[] Map { get; set; }
+
+        
+        public HashMap(int arrSize)
+        {
+            Map = new Dictionary<string, string>[arrSize];
+            
+            //UnNull every index in the Array
+            for (int i = 0; i < arrSize; i++)
+                Map[i] = new Dictionary<string, string>();
+}
+        
+        public int Hash(string key)
+        {   //Convert A Key to A Hash Number
+            return BitConverter.ToInt32(Encoding.Default.GetBytes(key), 0);
+        }
+        
+        public int Compress(int hash)
+        {   //Compresses The Hash Number
+            //To Fit Inside The Array
+            return hash % Map.Length;
+        }
+        
+        public void Assign(string key, string value)
+        {
+            if (key.Length < 4) return;
+            
+            //Generate A Hash Number And
+            //Compress It To Get An Index
+            int index = Compress(Hash(key));
+            Map[index][key] = value;
+        }
+       
+        public string Retrive(string key)
+        {
+            int index = Compress(Hash(key));
+            return Map[index][key];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blossom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int HashMapSize = 20;
+            HashMap hashMap = new HashMap(HashMapSize);
+
+            hashMap.Assign("Key_Example", "Value_Example");
+            //Fill Dictionary With Random 
+            //Keys And Values
+            for (int i = 0; i < 1000000; i++)
+            {
+                hashMap.Assign(GetRandomString(), GetRandomString());
+            }
+
+        }
+        public static string GetRandomString()
+        {
+            string word = "";
+            string letters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+            for(int i = new Random().Next(4,10); i > 0; i--)
+            {
+                word += letters[new Random().Next(0, letters.Length)];
+            }
+            return word;
+        }
+    }
+}
